f948b0ccfba99741a736d621520f11770d004bba
rohan <rohanthakur89768@gmail.com>
2025-07-08 08:16:31 +0530

    subsquence

---
+++ leetcode150_tp/issubsequcence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int n , m;
+    bool f(int i, int j, string &s , string & t){
+        if( i == n  ) return true;
+
+        if(j == m ) return false;
+
+        if(s[i] == t[j]) return f(i+1, j+1, s, t);
+        return f(i , j+1, s, t);
+    }
+    bool isSubsequence(string s, string t) {
+        n = s.size();
+        m = t.size();
+        return f(0, 0 , s ,t);
+    }
+};
