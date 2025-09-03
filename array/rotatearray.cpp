ee2b5a2841a39d58c2955bacb9ee0f6fee805526
rohan <rohanthakur89768@gmail.com>
2025-09-03 07:58:08 +0530

    rotate arry

---
+++ array/rotatearray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+   void f(int i, int j, vector<int>& nums ){
+       while(i<j){
+           swap(nums[i] , nums[j]);
+           i++;
+           j--;
+       }
+   }
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        k = k%n;
+        f(0, n-k-1, nums);
+        f(n-k, n-1, nums);
+        f(0, n-1, nums);
+    }
+};
